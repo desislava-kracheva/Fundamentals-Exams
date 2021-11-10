e79ce040176861631e1e924400e1dcbfb054f6ed
Desislava Kracheva <desislava.92@abv.bg>
2021-11-10 12:20:09 +0200

    ex. from exam

---
+++ softuniReception.js
@@ -0,0 +1,28 @@
+function solve(arr){
+
+    arr = arr.map(Number);
+let employees = 3;
+
+let efficientyFirst = arr.shift();
+let efficientySecond = arr.shift();
+let efficientyThird = arr.shift();
+let hour = 0;
+
+let efficientyPerHour = efficientyFirst + efficientySecond + efficientyThird;
+
+let students = arr.shift();
+
+while(students > 0){
+
+    hour++;
+    if(hour % 4 !== 0){
+    students -= efficientyPerHour;
+
+    }
+}
+
+console.log(`Time needed:  ${hour}h.`);
+
+}
+
+solve(['5','6','4','20'])
